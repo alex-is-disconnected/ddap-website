3e5dfbcedb57ca26a3e38868c89ba597215c9ede
Minkyung Cho <112492904+miincho@users.noreply.github.com>
2024-10-29 03:07:43 -0400

    moved headshots

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,54 +5,54 @@ const Team = () => {
   const teamMembers = [
     {
       headshot:
-        "../src/polaroids/Jennifer Lee - President.png",
+        "/polaroids/Jennifer Lee - President.png",
       name: "Jennifer Lee",
       title: "President",
       id: "pic1",
     },
     {
-      headshot: "../src/polaroids/Deeva Thomas - Vice President.png",
+      headshot: "/polaroids/Deeva Thomas - Vice President.png",
       name: "Deeva Thomas",
       title: "Vice President",
       id: "pic2",
     },
     {
       headshot:
-        "../src/polaroids/Mark Wong - Designer.png",
+        "/polaroids/Mark Wong - Designer.png",
       name: "Mark Wong",
       title: "Head of Design",
       id: "pic3",
     },
     {
-      headshot: "../src/polaroids/Ketana Anekar - Treasurer.png",
+      headshot: "/polaroids/Ketana Anekar - Treasurer.png",
       name: "Ketana Anekar",
       title: "Treasurer",
       id: "pic4",
     },
     {
       headshot:
-        "../src/polaroids/Minkyung Cho - Developer.png",
+        "/polaroids/Minkyung Cho - Developer.png",
       name: "Minkyung Cho",
       title: "Developer",
       id: "pic5",
     },
     {
       headshot:
-        "../src/polaroids/charis pao.png",
+        "/polaroids/charis pao.png",
       name: "Charis Pao",
       title: "Media Director",
       id: "pic6",
     },
     {
       headshot:
-        "../src/polaroids/Reika Oh - Media Director.png",
+        "/polaroids/Reika Oh - Media Director.png",
       name: "Reika Oh",
       title: "Media Director",
       id: "pic7",
     },
     {
       headshot:
-        "../src/polaroids/Joseph Crosthwaite - Operations.png",
+        "/polaroids/Joseph Crosthwaite - Operations.png",
       name: "Joseph Crosthwaite",
       title: "Operations",
       id: "pic8",
